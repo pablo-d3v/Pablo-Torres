9aa4cfd69bad5c28b3bd827ce36c49a4a96e2bf8
pablo- d3v <pablo.developer001@gmail.com>
2025-05-02 23:37:12 +0000

    ToDo

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ My Personal Portfolio
 # ToDo
 - Studies
 - nav bar Sticky
+- URL in variables
 - Change proyects-template.html title
 - Images' lightbox
--- config.js
+++ config.js
@@ -2,8 +2,7 @@ export const Paths = {
     root: '/',
     projects: '/projects/project-template.html',
     studies: '/studies.html',
-    contact: '/contact.html',
-    
-    css: '/style.css',
+    contact: './contact/contact.html',    
+    css: '/style.css'
     // Add all paths here
-  };
+  };    
